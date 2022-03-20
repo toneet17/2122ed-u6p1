eaa3e609b98470ac0e943d0e77e812839179b0c8
toneet17 <antonimedinadam@gmail.com>
2022-03-20 21:32:52 +0100

    "[MOD] exercici 3 fet"

--- src/main/java/ed/antoni/Institut.java
+++ src/main/java/ed/antoni/Institut.java
@@ -2,14 +2,19 @@ package ed.antoni;
 
 public class Institut {
 
+	private static final int ANY = 2022;
+	private static final String CICLE_FORMATIU = "1º DAM";
+	private static final String COGNOMS_ALUMNE = "Medina";
+	private static final String NOM_ALUMNE = "Antoni";
+
 	public static void main(String[] args) {
 		
 		Alumne p = new Alumne();
 		
-		p.nom = "Antoni";
-		p.cognoms = "Medina";
-		p.cicleFormatiu = "1º DAM";
-		p.any = 2022;
+		p.nom = NOM_ALUMNE;
+		p.cognoms = COGNOMS_ALUMNE;
+		p.cicleFormatiu = CICLE_FORMATIU;
+		p.any = ANY;
 		
 		System.out.println(p.toString());
 
